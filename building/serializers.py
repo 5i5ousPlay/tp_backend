0aabce140e51f93e8f59a3445fe40ff6d78481c0
RJ Conanan <rauljarodconanan@gmail.com>
2024-02-27 17:49:02 +0800

    display associated restrooms in building

--- building/serializers.py
+++ building/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Building
+from restroom.serializers import RestroomSerializer
 
 
 class BuildingSerializer(serializers.ModelSerializer):
+    restrooms = RestroomSerializer(many=True)
+
     class Meta:
         model = Building
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'restrooms']
