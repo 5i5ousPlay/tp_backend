ebd8e03079c49e40b0639b6c80556518890c7d93
RJ Conanan <rauljarodconanan@gmail.com>
2024-02-27 20:50:57 +0800

    add review app urls to global urls

--- tp_backend/urls.py
+++ tp_backend/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path(f'{base_url}building/', include('building.urls')),
     path(f'{base_url}restroom/', include('restroom.urls')),
     path(f'{base_url}rating/', include('rating.urls')),
+    path(f'{base_url}review/', include('review.urls')),
 ]
