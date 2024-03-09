a167c4d3f8aea2e7b7ee13dd1807a4963d98b076
RJ Conanan <rauljarodconanan@gmail.com>
2024-03-09 16:44:29 +0800

    configure global urls for image module

--- tp_backend/urls.py
+++ tp_backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 base_url = "api/v1/"
@@ -28,4 +30,8 @@ urlpatterns = [
     path(f'{base_url}rating/', include('rating.urls')),
     path(f'{base_url}review/', include('review.urls')),
     path(f'{base_url}tag/', include('tag.urls')),
+    path(f'{base_url}image/', include('image.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
