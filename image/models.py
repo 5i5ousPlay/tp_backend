a6e622571b8b285cdcd8f457410eceeb371e468d
RJ Conanan <rauljarodconanan@gmail.com>
2024-03-09 22:35:42 +0800

    added image module

---
+++ image/models.py
@@ -0,0 +1,26 @@
+import uuid
+import os
+from django.db import models
+from restroom.models import Restroom
+from review.models import Review
+from building.models import Building
+from user.models import CustomUser as User
+from core.utils.functions import get_image_upload_path
+
+
+# Create your models here.
+class Image(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    restroom = models.ForeignKey(Restroom, on_delete=models.CASCADE, null=True, related_name='images')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, null=True, related_name='images')
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, null=True, related_name='images')
+    image = models.ImageField(upload_to=get_image_upload_path, max_length=255)
+    uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    uploaded_on = models.DateTimeField(auto_now_add=True, null=True)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+
+        super(Image, self).delete(*args, **kwargs)
---
+++ image/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from .models import Image
+from .serializers import ImageSerializer
+from .permissions import IsImageOwner
+
+
+# Create your views here.
+class ImageUploadView(generics.CreateAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ImageListView(generics.ListAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [AllowAny]
+
+
+class ImageDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [IsAuthenticated & (IsImageOwner | IsAdminUser)]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        else:
+            return super().get_permissions()
+
+
+class ImageDeleteView(generics.DestroyAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [IsAuthenticated & (IsImageOwner | IsAdminUser)]
