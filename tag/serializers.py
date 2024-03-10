c4d5ff40e9c824b533112f46f54e01372358878a
RJ Conanan <rauljarodconanan@gmail.com>
2024-03-10 16:28:45 +0800

    prevent creation of duplicate tags of default tags

--- tag/serializers.py
+++ tag/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import Tag
+from django.conf import settings
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+    def create(self, validated_data):
+        name = validated_data['name']
+        if name in settings.DEFAULT_TAGS:
+            raise serializers.ValidationError(
+                {"detail": "The name provided is a default tag. Please specify a different name."}
+            )
+
+        return super().create(validated_data)
