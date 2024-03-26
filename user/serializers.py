9ae72646bbb16b35c8f67ca4fea4f8238e4680dc
RJ Conanan <rauljarodconanan@gmail.com>
2024-03-26 12:44:51 +0800

    moved user serializer to shared serializer file

--- user/serializers.py
+++ user/serializers.py
@@ -1,20 +1,12 @@
 from rest_framework import serializers
 from .models import CustomUser as User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'password']
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+from review.serializers import ReviewSerializer
+from core.utils.serializers import UserSerializer
 
 
 class UserProfileSerializer(UserSerializer):
+    reviews = ReviewSerializer(many=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'reviews', 'is_staff']
