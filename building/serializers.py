cd2afadd4222be2eafed817dad1ea7c3befbd92e
RJ Conanan <rauljarodconanan@gmail.com>
2024-03-26 12:57:37 +0800

    display building images

--- building/serializers.py
+++ building/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Building
 from restroom.serializers import RestroomSerializer
+from image.serializers import ImageSerializer
 
 
 class BuildingSerializer(serializers.ModelSerializer):
@@ -11,4 +12,5 @@ class BuildingSerializer(serializers.ModelSerializer):
 
 
 class BuildingGETSerializer(BuildingSerializer):
-    restrooms = RestroomSerializer(many=True)
+    restrooms = RestroomSerializer(many=True, read_only=True)
+    images = ImageSerializer(many=True, read_only=True)
